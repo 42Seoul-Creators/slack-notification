17cbc310293c4f170cd271a12a438b68a2127b4e
joonhan <joonhan@student.42seoul.kr>
2024-08-29 10:56:13 +0900

    ✨ feat: 사용자 계정 대신 GitHub App 이 이슈 생성하도록 변경

--- src/modules/env.js
+++ src/modules/env.js
@@ -4,7 +4,9 @@ const env = {
   github: {
     owner: process.env.OWNER,
     repo: process.env.REPO,
-    auth: process.env.TOKEN,
+    appId: process.env.APP_ID,
+    installationId: process.env.APP_INSTALLATION_ID,
+    privateKey: process.env.APP_PRIVATE_KEY,
   },
 };
 
--- src/modules/github.js
+++ src/modules/github.js
@@ -1,36 +1,35 @@
-import { Octokit } from "octokit";
+import { Octokit } from "@octokit/rest";
+import { createAppAuth } from "@octokit/auth-app";
 
 import env from "./env.js";
 import { getFormatTodayDate, getWeek } from "./date.js";
 import logger from "./logger.js";
 
-const octokit = new Octokit({
-  auth: env.github.auth,
+const appOctokit = new Octokit({
+  authStrategy: createAppAuth,
+  auth: {
+    appId: env.github.appId,
+    privateKey: env.github.privateKey,
+    installationId: env.github.installationId,
+  },
 });
-
-const OWNER = env.github.owner;
-const REPO = env.github.repo;
 
 const year = getFormatTodayDate("YYYY년");
 const month = getFormatTodayDate("MM월");
 
 export const createGithubIssue = async (nofiticationMessage) => {
   try {
-    const response = await octokit.request(
-      `POST /repos/${OWNER}/${REPO}/issues`,
-      {
-        owner: "OWNER",
-        repo: "REPO",
-        title: `${getFormatTodayDate(
-          "YYYY년 MM월"
-        )} ${getWeek()} 주차 인증 스레드`,
-        body: nofiticationMessage,
-        labels: [`${year} ${month}`],
-        headers: {
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      }
-    );
+    const { owner, repo } = env.github;
+
+    const response = await appOctokit.issues.create({
+      owner,
+      repo,
+      title: `${getFormatTodayDate(
+        "YYYY년 MM월"
+      )} ${getWeek()} 주차 인증 스레드`,
+      body: nofiticationMessage,
+      labels: [`${year} ${month}`],
+    });
     logger.info(response.data);
     logger.info("GitHub 이슈를 성공적으로 생성했습니다.");
   } catch (error) {
